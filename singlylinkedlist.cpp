091d88b4962eaed841600fbc5fc8686b7525cbf5
Pranjal-96 <142165521+Pranjal-96@users.noreply.github.com>
2023-08-23 23:08:45 +0530

    Update singlylinkedlist.cpp

--- singlylinkedlist.cpp
+++ singlylinkedlist.cpp
@@ -1,247 +1,105 @@
 #include<iostream>
+
 using namespace std;
+template < typename T >
+    class Node {
+        public: T data;
+        Node * next;
+        Node() {
+            data = 0;
+            next = NULL;
+        }
 
-struct node{
-    
-    int data;
-    struct node*next;
-};
+        Node(T data) {
+            this -> data = data;
+            this -> next = NULL;
+        }
+    };
+template < typename T >
 
-struct node* createlinkedlist(int n)
-{
-
-struct node *head=NULL,*tail=NULL;
-int i;
-for(i=1;i<=n;i++)
-   {
-struct node* temp= (struct node*)malloc(sizeof(struct node*));
-
-cout<<"enter the data"<<endl;
-cin>>temp->data;
-temp->next=NULL;
-
-
-          if(head==NULL)
-           {
-              head=temp;
-              tail=temp;
-           }
-           else
-           {
-              tail->next=temp;
-              tail=temp;
+    class SinglyLinkedList {
+        Node < T > * head;
+        public:
+            void insertionAtEnd(T data) {
+                Node < T > * newnode = new Node < T > (data);
+                if (head == NULL) {
+                    head = newnode;
+                    return;
+                }
+                Node < T > * temp = head;
+                while (temp -> next != NULL) {
+                    temp = temp -> next;
+                }
+                temp -> next = newnode;
             }
+        void searchElement(T data) {
+            Node < T > * temp = head;
+            while (temp -> data != data && temp -> next != NULL)
+                temp = temp -> next;
+            if (temp -> data == data)
+                cout << "element present" << endl;
+            else
+                cout << "element not present" << endl;
+
+        }
+        void deleteElement(int data) {
+            Node < T > * temp = head;
+            Node < T > * previous = NULL;
+            while (temp -> data != data && temp -> next != NULL) {
+                previous = temp;
+                temp = temp -> next;
+            }
+            if (previous == NULL)
+                head = head -> next;
+            else if (temp -> data == data)
+                previous -> next = temp -> next;
+            else
+                cout << "element not present" << endl;
+        }
+        void print() {
+            Node < T > * temp = head;
+            while (temp != NULL) {
+                cout << temp -> data << " ";
+                temp = temp -> next;
+            }
+        }
+    };
+
+int main() {
+    SinglyLinkedList < int > list;
+    int operation;
+    int data;
+    while (1) {
+        cout << "Select Operation To Perform on linked list" << endl;
+        cout << "1.Insert" << endl;
+        cout << "2.Search" << endl;
+        cout << "3.Delete" << endl;
+        cout << "4. Exit" << endl;
+        cin >> operation;
+        switch (operation) {
+        case 1:
+            cout << "enter element to be inserted" << endl;
+            cin >> data;
+            list.insertionAtEnd(data);
+            list.print();
+            cout << endl;
+            break;
+        case 2:
+            cout << "enter element to be searched" << endl;
+            cin >> data;
+            list.searchElement(data);
+            list.print();
+            cout << endl;
+            break;
+        case 3:
+            cout << "enter element to be deleted" << endl;
+            cin >> data;
+            list.deleteElement(data);
+            list.print();
+            cout << endl;
+            break;
+        case 4:
+            return 0;
+        }
     }
-
-return head;
-    
 }
-
-
-
-void print(struct node* head)
-{
-
-cout<<"element of the list are"<<endl;
-
-
-while(head!=NULL)
-     {
-      cout<<head->data<<endl;
-      head=head->next;
-      }
-
-}
-
-
-
-
-
-int search(struct node*head,int k)
-{
-struct node *trav=head;
-
-while(trav!=NULL)
-{
-
-if(trav->data==k)
-   return 1;
-
-trav=trav->next;
-}
-
-return 0;
-}
-
-
-struct node* insert(struct node*head,int pos)
-   
-   {
-      int len=0;
-         
-     struct node*trav=head;
-         
-     struct node* temp= (struct node*)malloc(sizeof(struct node*));
-
-          cout<<"enter the data you want to insert"<<endl;
-          cin>>temp->data;
-          temp->next=NULL;
-          
- //finding the length
-      while(trav!=NULL)
-          {
-              len++;
-              trav=trav->next;
-          }
-          
- //insert at begining
- 
-      if(pos==0)
-      {
-      temp->next=head;
-      head=temp;
-      
-      }
-  //insert at end    
-      else if(pos==len-1)
-      {
-      trav=head;
-          while(trav->next!=NULL)
-          {
-          trav=trav->next;
-          }
-        trav->next=temp;
-      }
- //insert elsewhere
-   else
-   {
-     trav=head;
-      for(int i=0;i<pos-1;i++)
-       {
-       
-       trav=trav->next;
-       
-       }
-  
-  temp->next=trav->next;
-  trav->next=temp;
-  
-   }
-   
-   return head;
-}
-
-
-struct node* deletell(struct node*head,int posdelete)
-{
-
-int len=0;
-
-
-struct node*trav=head,*prev=NULL;
-
- //finding the length
-      while(trav!=NULL)
-          {
-              len++;
-              trav=trav->next;
-          }
-
-//delete from begining
-       if(posdelete==0)
-       {
-           
-           head=head->next;
-           
-           
-       }
- //delete from end      
-       else if(posdelete==len-1)
-       {
-           trav=head;
-           
-          while(trav->next!=NULL)           
-           {
-               
-           prev=trav;
-           trav=trav->next;
-               
-               
-           }
-           
-           prev->next=trav->next;
-           
-        }
-  // delete from elsewhere
-        else
-        {
-            trav=head;
-            
-           for(int i=0;i<posdelete;i++)
-           {
-               prev=trav;
-               trav=trav->next;
-               
-           }
-           
-           
-           
-           prev->next=trav->next;
-           
-           
-        }
-        
-        
-       return head;
-    
-    
-
-}
-
-int main()
-{
-
-int n,i,x,pos,posdelete;
-
-cout<<"enter the number of nodes"<<endl;
-
-cin>>n;
-
-struct node*head=createlinkedlist(n);
-print(head);
-
-cout<<"enter the node you want search"<<endl;
-cin>>x;
-
-if(search(head,x))
-{
-cout<<"element is found"<<endl;
-}
-else
-{
-cout<<"element is not found"<<endl;
-}
-
-
-cout<<"enter the pos at which you want to insert"<<endl;
-
-cin>>pos;
-
-head=insert(head,pos);
-print(head);
-
-
-
-
-cout<<"enter the pos at which you want to delete the node"<<endl;
-
-cin>>posdelete;
-
-
-head=deletell(head,posdelete);
-print(head);
-
-
-return 0;
-}
-
